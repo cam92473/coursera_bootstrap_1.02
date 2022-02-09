4049a9a2592e1c65d87cfc75c005bbfe1b1261bf
pizzafrog <camdleahy@gmail.com>
2022-02-09 08:58:09 -0700

    grunt

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//Carousel
+$(document).ready(function() {
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+});
+
+//Login
+$(document).ready(function() {
+    $('#loginLink').click(function() {
+        $('#loginModal').modal('toggle')
+    });
+});
+
+//Reserve
+$(document).ready(function() {
+    $('#reserveButton').click(function() {
+        $('#reserveModal').modal('toggle')
+    });
+});
